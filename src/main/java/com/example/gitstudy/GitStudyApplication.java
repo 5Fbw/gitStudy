776b70dabb68f99d42dd833ab25057a0a41443e0
5Fbw <565826372@qq.com>
2024-06-20 13:54:04 +0800

    first modify

--- src/main/java/com/example/gitstudy/GitStudyApplication.java
+++ src/main/java/com/example/gitstudy/GitStudyApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GitStudyApplication {
-
+//第一次修改
     public static void main(String[] args) {
         SpringApplication.run(GitStudyApplication.class, args);
     }
